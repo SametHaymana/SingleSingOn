588ec7e233a0fbfa62ba4f7a610e1a71e6fcf67c
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-02 01:18:51 +0300

    Add repository interfaces for Client and User models

---
+++ src/main/java/com/sso/api/repositories/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.sso.api.repositories;
+
+import com.sso.api.models.Client;
+
+
+public interface ClientRepository extends CrudRepository<Client, Integer> {}
---
+++ src/main/java/com/sso/api/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.sso.api.repositories;
+
+import com.sso.api.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {}
