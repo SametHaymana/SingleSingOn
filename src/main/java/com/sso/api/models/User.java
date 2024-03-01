3c7c99aefda677fb1f2603909ffd8647d89d69f4
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-02 01:09:49 +0300

    Add annotations and fields to User model, metas relations and mroe

--- src/main/java/com/sso/api/models/User.java
+++ src/main/java/com/sso/api/models/User.java
@@ -3,12 +3,18 @@ package com.sso.api.models;
 import java.util.UUID;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,4 +25,50 @@ public class User {
     private Long id;
 
     private UUID uid = UUID.randomUUID();
+
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String username;
+
+    // Email is unique
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+
+    // Password is hashed
+    @Column(nullable = false, length = 256)
+    private String password;
+
+
+    /* User Info */
+
+    @Column(nullable = true, length = 100)
+    private String firstName;
+
+    @Column(nullable = true, length = 100)
+    private String lastName;
+
+    @Column(nullable = true, length = 100)
+    private String phone;
+
+
+    /* Relations */
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client; 
+
+
+    /* Meta */
+
+    @CreatedDate
+    private Long createdAt;
+
+    @UpdateTimestamp
+    private Long updatedAt;
+
+    @SoftDelete
+    private Long deletedAt;
+
+
 }
