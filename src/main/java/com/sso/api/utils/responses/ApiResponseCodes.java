a6be7201727440be0d387c1477e6b5a81e2b423a
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-03 18:18:53 +0300

    Add ApiResponseCodes enum

---
+++ src/main/java/com/sso/api/utils/responses/ApiResponseCodes.java
@@ -0,0 +1,21 @@
+package com.sso.api.utils.responses;
+
+import org.hibernate.query.Page;
+
+import lombok.Getter;
+
+@Getter
+public enum ApiResponseCodes {
+    InternalServerError(50000, "Internal server error"),
+    PageNotFound(40400, "Page not found"),
+    ClientNotFound(40401, "Client not found"),
+    UserNotFound(40401, "User not found");
+
+    private final int code;
+    private final String message;
+
+    ApiResponseCodes(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
