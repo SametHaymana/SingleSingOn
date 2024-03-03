0f77bc13fa09da5f85b5e466fd14e47808c085bd
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-03 18:20:06 +0300

    Update validation annotation for clientId

--- src/main/java/com/sso/api/modules/auth/dtos/ClientCheckRequestDto.java
+++ src/main/java/com/sso/api/modules/auth/dtos/ClientCheckRequestDto.java
@@ -3,12 +3,13 @@ package com.sso.api.modules.auth.dtos;
 import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ClientCheckRequestDto {
     
-    @NotBlank(message = "clientId is required")
+    @NotNull(message = "clientId is required")
     private UUID clientId;
 
     @NotBlank(message = "client redirect url is required")
