ccbc03aa7e505750e1af0e3607d2ffbdc9f721c0
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-03 18:19:45 +0300

    Add GlobalExceptionHandler class for handling exceptions

---
+++ src/main/java/com/sso/api/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.sso.api.handlers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sso.api.utils.responses.ApiError;
+import com.sso.api.utils.responses.ApiResponseCodes;
+import com.sso.api.utils.responses.ApiErrors.InternalServerError;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> handleException(Exception e) {
+        ApiError err;
+        if (e instanceof ApiError) {
+            err = (ApiError) e;
+        } else {
+            // Log the exception
+            e.printStackTrace();
+            // Fallback to Internal Server Error
+            err = new InternalServerError(ApiResponseCodes.InternalServerError);
+        }
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("status", err.getStatus());
+        responseBody.put("body", new HashMap<String, Object>() {{
+            put("apiError", err.getBody().getApiError());
+            put("message", err.getBody().getMessage());
+        }});
+    
+        return ResponseEntity
+                .status(err.getStatus())
+                .body(responseBody);
+    }
+    
+}
