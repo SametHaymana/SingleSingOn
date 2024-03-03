ab59f2efbb3764c755af491c70db1eff75da548f
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-03 19:12:45 +0300

    Add API response codes for 404 and 400 errors

--- src/main/java/com/sso/api/utils/responses/ApiResponseCodes.java
+++ src/main/java/com/sso/api/utils/responses/ApiResponseCodes.java
@@ -1,14 +1,22 @@
 package com.sso.api.utils.responses;
 
 import lombok.Getter;
-import org.hibernate.query.Page;
 
 @Getter
 public enum ApiResponseCodes {
   InternalServerError(50000, "Internal server error"),
+
+  /*    NotFound 404    */
   PageNotFound(40400, "Page not found"),
   ClientNotFound(40401, "Client not found"),
-  UserNotFound(40401, "User not found");
+  UserNotFound(40401, "User not found"),
+
+  /*    Forbbiden 403    */
+
+  /*    Unauthorized 401  */
+
+  /*    BadRequest 400    */
+  RedirectUriNotValid(40000, "Redirect uri not valid");
 
   private final int code;
   private final String message;
