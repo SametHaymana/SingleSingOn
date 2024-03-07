55975841823f4c7e252535b692412298db1a5836
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-07 23:39:09 +0300

    Refactor Hasher class to use Argon2PasswordEncoder and add SHA-256 hashing method

--- src/main/java/com/sso/api/utils/Hasher.java
+++ src/main/java/com/sso/api/utils/Hasher.java
@@ -1,19 +1,32 @@
 package com.sso.api.utils;
 
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.bouncycastle.util.encoders.Hex;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 public final class Hasher {
-  private static final Argon2PasswordEncoder encoder;
+  private static final Argon2PasswordEncoder encoderArgon2;
 
   static {
-    encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    encoderArgon2 = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
   }
 
-  public static String hash(String password) {
-    return encoder.encode(password);
+  public static String pass_hash(String password) {
+    return encoderArgon2.encode(password);
   }
 
-  public static boolean verify(String password, String hashedPassword) {
-    return encoder.matches(password, hashedPassword);
+  public static boolean pass_verify(String password, String hashedPassword) {
+    return encoderArgon2.matches(password, hashedPassword);
+  }
+
+  public static String hashSHA256(String data) throws NoSuchAlgorithmException  {
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    byte[] hash = digest.digest(
+      data.getBytes(StandardCharsets.UTF_8));
+    return new String(Hex.encode(hash));
   }
 }
