45b8be69ff2ae8cd18a8da35a177616cc05afce6
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-10 13:46:06 +0300

    Add SsoProperties configuration to ApiApplicationTests

--- src/test/java/com/sso/api/ApiApplicationTests.java
+++ src/test/java/com/sso/api/ApiApplicationTests.java
@@ -1,9 +1,12 @@
 package com.sso.api;
 
+import com.sso.api.config.SsoProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableConfigurationProperties(SsoProperties.class)
 class ApiApplicationTests {
 
   @Test
