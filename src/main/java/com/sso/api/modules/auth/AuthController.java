f7c07efea1c93eee02e33eacfdd7fa3c16f299e9
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-10 13:45:48 +0300

    Add encryption and hashing services to AuthController, update login functionlality

--- src/main/java/com/sso/api/modules/auth/AuthController.java
+++ src/main/java/com/sso/api/modules/auth/AuthController.java
@@ -7,20 +7,20 @@ import com.sso.api.modules.auth.dtos.requests.UserLoginRequestDto;
 import com.sso.api.modules.auth.dtos.responses.ClientCheckResponseDto;
 import com.sso.api.modules.auth.dtos.responses.UserLoginResponseDto;
 import com.sso.api.modules.auth.services.ClientService;
+import com.sso.api.modules.auth.services.EncryptorService;
 import com.sso.api.modules.auth.services.UserService;
+import com.sso.api.utils.Hasher;
 import com.sso.api.utils.responses.ApiErrors.BadRequestError;
 import com.sso.api.utils.responses.ApiResponse;
 import com.sso.api.utils.responses.ApiResponseCodes;
 import jakarta.validation.Valid;
+import java.util.Base64;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,10 +28,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
   private final ClientService clientService;
   private final UserService userService;
+  private final EncryptorService encryptorService;
 
-  public AuthController(ClientService clientService, UserService userService) {
+  public AuthController(
+    ClientService clientService,
+    UserService userService,
+    EncryptorService encryptorService
+  ) {
     this.clientService = clientService;
     this.userService = userService;
+    this.encryptorService = encryptorService;
   }
 
   private boolean validateRedirectUri(Client client, String redirectUri) {
@@ -74,7 +80,8 @@ public class AuthController {
   @GetMapping("/login")
   public ResponseEntity<ApiResponse<UserLoginResponseDto>> login(
     @Valid @RequestBody UserLoginRequestDto body
-  ) {
+  )
+    throws Exception {
     Client client = clientService.getClient(body.getClientId());
 
     if (!this.validateRedirectUri(client, body.getRedirectUri())) {
@@ -85,7 +92,45 @@ public class AuthController {
     User user = this.userService.getUserByEmail(body.getEmail());
 
     // Validate user password
+    if (!Hasher.pass_verify(body.getPassword(), user.getPassword())) {
+      throw new BadRequestError(ApiResponseCodes.InvalidCredentials);
+    }
 
-    return null;
+    // TODO
+    // Check user other verification
+    // 2FA, email verification, etc
+
+    // Generate code
+    /*
+     *  Code Structure
+     *
+     *  created_at:expired_at:client_id:user_id:state:API_SECRET1:hash
+     *
+     *  as encypted base64
+     *
+     */
+
+    String code = "";
+    code += System.currentTimeMillis() + ":";
+    code += (System.currentTimeMillis() + 1000 * 60 * 5) + ":"; // 5 minutes
+    code += client.getUid() + ":";
+    code += user.getUid() + ":";
+    code += body.getState() + ":";
+    code += Hasher.hashSHA256(code);
+
+    // Encrypt code
+    code = this.encryptorService.encrypt(code);
+
+    // Update redirect uri
+    String updatedRedirectUri =
+      body.getRedirectUri() + "?code=" + code + "&state=" + body.getState();
+
+    // ToDo
+    // Save code to database
+    // Auditing
+    // Log user login
+    // etc
+
+    return ApiResponse.ok(new UserLoginResponseDto(updatedRedirectUri, body.getState()));
   }
 }
