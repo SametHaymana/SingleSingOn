bee163ca2afab31b2f0b964ad97c1cc346e8164e
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-03-10 13:46:02 +0300

    Add SsoProperties configuration

--- src/main/java/com/sso/api/ApiApplication.java
+++ src/main/java/com/sso/api/ApiApplication.java
@@ -1,9 +1,12 @@
 package com.sso.api;
 
+import com.sso.api.config.SsoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SsoProperties.class)
 public class ApiApplication {
 
   public static void main(String[] args) {
